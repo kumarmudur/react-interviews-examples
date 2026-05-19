370d871ac3c7c2aad275cd9ae9d6ce10ff50fbe3
kumarmudur <kumarmudur10@gmail.com>
2026-05-19 14:42:59 +0530

    useToggle example

--- src/components/ToggleButton/useToggle.js
+++ src/components/ToggleButton/useToggle.js
@@ -1,7 +1,8 @@
-import {useState} from "react";
+import { useState } from "react";
 
 export const useToggle = (initialValue) => {
     const [value, setValue] = useState(initialValue);
     const toggle = () => setValue((prev) => !prev);
     return [value, toggle];
 }
+
