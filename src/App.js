648ab28ea3becc71f7a21ee7a18c44fb3822baa5
kumarmudur <kumarmudur10@gmail.com>
2026-05-23 20:23:49 +0530

    app file changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import DarkModeToggle from "./components/darkMode/DarkModeToggle";
+//import DarkModeToggle from "./components/darkMode/DarkModeToggle";
 import TodoList from './components/todoList/TodoList';
 import InputOTP from './components/inputOTP/InputOTP';
+import EvenOrOdd from './components/EvenOrOdd';
+import ChipsInput from './components/ChipsInput';
+import ToggleButton from './components/ToggleButton';
 
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>ReactJS Examples</h1>
+      {/*<h1>ReactJS Examples</h1>*/}
       {/*<TodoList />*/}
       {/*  <DarkModeToggle />*/}
-        <InputOTP />
+      {/*  <InputOTP />*/}
+      {/*  <EvenOrOdd />*/}
+      {/*  <ChipsInput />*/}
+        <ToggleButton />
     </div>
   );
 }
